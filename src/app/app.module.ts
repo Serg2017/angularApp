f9b7a0e0cb829f8b20a5313ea0e91910ea721873
serg23 <serg22@gmail.com>
2017-09-07 00:53:29 +0300

    added new pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule} from '@angular/router';
+import {ArticleComponent} from './article/article.component';
+import {ContactComponent} from './contact/contact.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+      BrowserModule,
+      RouterModule.forRoot([
+          { path: '', component: HomeComponent },
+          { path: 'articles', component: ArticleComponent },
+          { path: 'contact', component: ContactComponent },
+      ]),
+  ],
+  declarations: [
+      AppComponent,
+      HomeComponent,
+      ArticleComponent,
+      ContactComponent,
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
