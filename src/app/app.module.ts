57c78f9de879831e8056457fd5a39e7b85b1e477
serg23 <serg22@gmail.com>
2017-09-07 03:32:01 +0300

    show table info birds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import {HomeComponent} from './home/home.component';
 import {RouterModule} from '@angular/router';
 import {ArticleComponent} from './article/article.component';
 import {ContactComponent} from './contact/contact.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports:      [
       BrowserModule,
+      FormsModule,
+      ReactiveFormsModule,
       RouterModule.forRoot([
           { path: '', component: HomeComponent },
           { path: 'articles', component: ArticleComponent },
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,38 @@ import {Component, OnInit} from '@angular/core';
 
 export class HomeComponent implements OnInit   {
 
+    visible = true;
+    birds = [
+        {id: 1, name: 'Crow', color: '#98FB98', likes: 0, dislike: 0},
+        {id: 2, name: 'Peacock', color: '#98FB98', likes: 0, dislike: 0},
+        {id: 3, name: 'Sparrow', color: '#98FB98', likes: 0, dislike: 0},
+        {id: 4, name: 'Cuckoo', color: '#98FB98', likes: 0, dislike: 0},
+        {id: 5, name: 'Eagle', color: '#98FB98', likes: 0, dislike: 0},
+        {id: 6, name: 'Swan', color: '#98FB98', likes: 0, dislike: 0},
+        {id: 7, name: 'Duck', color: '#98FB98', likes: 0, dislike: 0},
+        {id: 8, name: 'Hen', color: '#98FB98', likes: 0, dislike: 0},
+        {id: 9, name: 'Parrot', color: '#98FB98', likes: 0, dislike: 0},
+        {id: 10, name: 'Woodpecker', color: '#98FB98', likes: 0, dislike: 0}
+    ];
+    id = this.birds.length + 1;
     constructor() {}
     ngOnInit(): void {
     }
 
+    addLike(index): void {
+        this.birds[index].likes++;
+    }
+
+    addDisLike(index): void {
+        this.birds[index].dislike++;
+    }
+
+    remove(index): void {
+        this.birds.splice(index, 1);
+    };
+
+    addBird(name): void {
+        this.birds.push({id: this.id++, name: name, color: '#98FB98', likes: 0, dislike: 0});
+    }
 }
+
